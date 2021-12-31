3e768a179ebd4cdee279cf9faf8a26315f39e6cf
Javier Rostagno <88237623+jrostagno@users.noreply.github.com>
2021-12-31 16:31:06 -0300

    code refactor

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -4,9 +4,7 @@ import SearchBar from "../SearchBar/SearchBar";
 export default function Nav({ setCities, setNotFound, setIsLoading }) {
   return (
     <div className="flex justify-between bg-teal-900 items-center h-16">
-      <h2 className="text-xl text-slate-50 relative left-7">
-        The wheather App
-      </h2>
+      <h2 className="text-xl text-slate-50 relative left-7">The weather App</h2>
 
       <div className="relative right-7">
         <SearchBar
